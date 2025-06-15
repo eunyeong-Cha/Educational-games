b9f7191f9c974c7fade474e8a153e0290ce7a425
eunyeong-Cha <eunoiay0@naver.com>
2025-06-15 20:12:25 +0900

    pictureword-game, colorword-game 완성

---
+++ colorword-game/problems.js
@@ -0,0 +1,195 @@
+
+const problems = [
+  {
+    image: "images/그림_가지.jpg",
+    words: ["가지"]
+  },
+  {
+    image: "images/그림_개구리.jpg",
+    words: ["개구리"]
+  },
+  {
+    image: "images/그림_개미.jpg",
+    words: ["개미"]
+  },
+  {
+    image: "images/그림_거미.jpg",
+    words: ["거미"]
+  },
+  {
+    image: "images/그림_고구마.jpg",
+    words: ["고구마"]
+  },
+  {
+    image: "images/그림_고래.jpg",
+    words: ["고래"]
+  },
+  {
+    image: "images/그림_구두.jpg",
+    words: ["구두"]
+  },
+  {
+    image: "images/그림_기차.jpg",
+    words: ["기차"]
+  },
+  {
+    image: "images/그림_기타.jpg",
+    words: ["기타"]
+  },
+  {
+    image: "images/그림_나무.jpg",
+    words: ["나무"]
+  },
+  {
+    image: "images/그림_나비.jpg",
+    words: ["나비"]
+  },
+  {
+    image: "images/그림_다리.jpg",
+    words: ["다리"]
+  },
+  {
+    image: "images/그림_도토리.jpg",
+    words: ["도토리"]
+  },
+  {
+    image: "images/그림_두부.jpg",
+    words: ["두부"]
+  },
+  {
+    image: "images/그림_마스크.jpg",
+    words: ["마스크"]
+  },
+  {
+    image: "images/그림_매미.jpg",
+    words: ["매미"]
+  },
+  {
+    image: "images/그림_모기.jpg",
+    words: ["모기"]
+  },
+  {
+    image: "images/그림_모자.jpg",
+    words: ["모자"]
+  },
+  {
+    image: "images/그림_무지개.jpg",
+    words: ["무지개"]
+  },
+  {
+    image: "images/그림_바구니.jpg",
+    words: ["바구니"]
+  },
+  {
+    image: "images/그림_바나나.jpg",
+    words: ["바나나"]
+  },
+  {
+    image: "images/그림_바다.jpg",
+    words: ["바다"]
+  },
+  {
+    image: "images/그림_바지.jpg",
+    words: ["바지"]
+  },
+  {
+    image: "images/그림_버스.jpg",
+    words: ["버스"]
+  },
+  {
+    image: "images/그림_보라.jpg",
+    words: ["보라"]
+  },
+  {
+    image: "images/그림_사이다.jpg",
+    words: ["사이다"]
+  },
+  {
+    image: "images/그림_사자.jpg",
+    words: ["사자"]
+  },
+  {
+    image: "images/그림_새우.jpg",
+    words: ["새우"]
+  },
+  {
+    image: "images/그림_시소.jpg",
+    words: ["시소"]
+  },
+  {
+    image: "images/그림_아파트.jpg",
+    words: ["아파트"]
+  },
+  {
+    image: "images/그림_야구.jpg",
+    words: ["야구"]
+  },
+  {
+    image: "images/그림_여우.jpg",
+    words: ["여우"]
+  },
+  {
+    image: "images/그림_오리.jpg",
+    words: ["오리"]
+  },
+  {
+    image: "images/그림_오이.jpg",
+    words: ["오이"]
+  },
+  {
+    image: "images/그림_우유.jpg",
+    words: ["우유"]
+  },
+  {
+    image: "images/그림_자두.jpg",
+    words: ["자두"]
+  },
+  {
+    image: "images/그림_지우개.jpg",
+    words: ["지우개"]
+  },
+  {
+    image: "images/그림_커피.jpg",
+    words: ["커피"]
+  },
+  {
+    image: "images/그림_쿠키.jpg",
+    words: ["쿠키"]
+  },
+  {
+    image: "images/그림_타조.jpg",
+    words: ["타조"]
+  },
+  {
+    image: "images/그림_토마토.jpg",
+    words: ["토마토"]
+  },
+  {
+    image: "images/그림_파리.jpg",
+    words: ["파리"]
+  },
+  {
+    image: "images/그림_포도.jpg",
+    words: ["포도"]
+  },
+  {
+    image: "images/그림_피아노.jpg",
+    words: ["피아노"]
+  },
+  {
+    image: "images/그림_피자.jpg",
+    words: ["피자"]
+  },
+  {
+    image: "images/그림_하나.jpg",
+    words: ["하나"]
+  },
+  {
+    image: "images/그림_하마.jpg",
+    words: ["하마"]
+  },
+  {
+    image: "images/그림_호두.jpg",
+    words: ["호두"]
+  }
+];
